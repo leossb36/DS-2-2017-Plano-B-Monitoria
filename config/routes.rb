d54c9e80688c40fa9d8625738d41933965e93ac2
Leonardo Dos Santos Silva Barreiros <leossb36@gmail.com>
2017-10-30 20:45:46 -0200

    Creating default layout and creating home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index'
+
   resources :services
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
