c4c657afcc5f14a995f1ae534472c429f5a0e343
joaovitor3 <joao_vitor_04@hotmail.com>
2017-11-19 17:25:57 -0200

    Add titulo to infos form

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -69,6 +69,6 @@ class InfosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def info_params
-      params.require(:info).permit(:texto, :image)
+      params.require(:info).permit(:titulo, :texto, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119173728) do
+ActiveRecord::Schema.define(version: 20171119191354) do
 
   create_table "infos", force: :cascade do |t|
     t.text "texto"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171119173728) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.string "titulo"
     t.index ["User_id"], name: "index_infos_on_User_id"
   end
 
