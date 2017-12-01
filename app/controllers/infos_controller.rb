e1b933de4478f067f03fa5d52c57964e84254c95
joaovitor3 <joao_vitor_04@hotmail.com>
2017-12-01 00:01:18 -0200

    Fix errors in infos controller

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,8 +1,10 @@
 class InfosController < ApplicationController
   # before_action :authenticate_user!, :except => [:index]
-  before_action :notice=> 'you must sign in first!' do
+  before_action :set_info, only: [:show, :edit, :update, :destroy]
+  before_action :except =>[:show] do
     redirect_to root_path unless current_user && current_user.is_admin?
   end
+
 
   # GET /infos
   # GET /infos.json
@@ -70,6 +72,7 @@ class InfosController < ApplicationController
     def set_info
       @info = Info.find(params[:id])
     end
+    
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def info_params
